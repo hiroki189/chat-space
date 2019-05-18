786bd12c215b1d1035a4abb174d599e83caa712f
hiroki189 <hiroki@hirokinoMacBook-Pro.local>
2019-05-18 21:30:17 +0900

    非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,48 @@
+$(function(){
+  function buildHTML(message){
+  var html = `<div class = "message">
+                <div class = "upper-messsage">
+                  <div class = "upper-message__user-name">
+                    ${ message.user_name}
+                  </div>
+                  <div class = "upper-message__date">
+                    ${ message.time }
+                  </div>
+                </div>
+                <div class = " message__text">
+                  <p class = "message__text__content">
+                    ${ message.content}
+                  </p>
+                </div>
+            </div>`;
+        return html;
+  }
+  function scroll(){
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+  }
+
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__input-box_text').val('');
+      $('.form__input-box_submit').prop('disabled',false);
+      scroll()
+    })
+    .fail(function(){
+      alert('メッセージを入力してください')
+      $('.form__input-box_submit').prop('disabled',false);
+    })
+  })
+})
