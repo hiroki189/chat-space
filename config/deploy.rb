9f8e48ff7667fc7318840727d0dabf0959d8e12c
hiroki189 <hiroki@hirokinoMacBook-Pro.local>
2019-06-05 19:36:58 +0900

    Fsww

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,20 +17,8 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
 
 after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
-
   desc 'upload secrets.yml'
   task :upload do
     on roles(:app) do |host|
@@ -42,7 +30,14 @@ end
   end
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
-end
+  end
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 
 
 
